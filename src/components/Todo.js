eb6cc55abc9c32cf91dfca398d5977781663beff
Lina <lbyland4402@students.mc3.edu>
2023-03-30 13:16:07 -0400

    commit kanban edit button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -22,37 +22,12 @@ function Todo(props) {
           <button type="button" className="btn btn__danger" onClick={() => props.deleteTask(props.id)}>
               Delete <span className="visually-hidden">{props.name}</span>
           </button>
-      </div>
-    </li>
-    );
-/* edit button for later...
-    return (
-      <li className="todo stack-small">
-      <div className="c-cb">
-      <input
-          id={props.id}
-          type="checkbox"
-          defaultChecked={props.completed}
-          onChange={() => props.toggleTaskCompleted(props.id)}
-        />
-        <label className="todo-label" htmlFor={props.id}>
-        {displayDate}: {props.name}
-        </label>
-      </div>
-      
-     
-        <button type="button" className="btn">
-          Edit <span className="visually-hidden">{props.name}</span>
+          <button type="button" className="btn">
+            Edit <span className="visually-hidden">{props.name}</span>
         </button>
-      
-        
-        
-      
-      
+      </div>
     </li>
-
     );
-  */
 
 }
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 //format(new Date(), "MM-dd-yyyy")
 const DATA = [
   { id: uuidv4(), name: "Eat", date: new Date(), completed: true },
-  { id: "todo-1", name: "Sleep", dateAdded: Date.parse('01 Jan 1960 00:00:00 GMT'), completed: false },
-  { id: "todo-2", name: "Repeat", dateAdded: Date.now(), completed: false }
+  { id: uuidv4(), name: "Sleep",date: new Date(), completed: false },
+  { id: uuidv4(), name: "Repeat", date: new Date(), completed: false }
 ];
 const sortedData = DATA.sort((a, b) => b.dateAdded - a.dateAdded);
 
